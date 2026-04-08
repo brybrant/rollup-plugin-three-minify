162ee91466422767e9b172e92893a55f60541a81
Matt Bryant <brybrant@gmail.com>
2026-04-08 10:40:01 +0100

    Use the `object` form of the `load` hook to set `order: pre` and filter only three modules.

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,10 @@ import removeShadows from './remove-shadows';
 import removeTextures from './remove-textures';
 import removeXR from './remove-xr';
 
+const threeRegex = /\/node_modules\/three\/build\/three\.[\w.]+$/;
+
 /**
- * Rudimentary plugin to minify shaders and remove redundant THREE.js code
+ * Minifies GLSL code in THREE.js and removes unused subsystems of `WebGLRenderer`
  * @param userOptions See [options](./options.ts)
  * @returns Rollup plugin
  */
@@ -28,108 +30,104 @@ export default function (userOptions: UserOptions = {}): Plugin {
 
   return {
     name,
-    // enforce: 'pre',
-    async load(path) {
-      const parsedPath = path.replace(/\\/g, '/');
-
-      if (!parsedPath.includes('node_modules/three/build/')) return null;
-
-      if (parsedPath.endsWith('.min.js')) {
-        console.warn(
-          `"${name}" does not work with the minified versions of THREE.js`,
-        );
-        return null;
-      }
-
-      const webglModule = parsedPath.endsWith('three.module.js');
-
-      let code: string;
-
-      try {
-        code = await readFile(path, 'utf8');
-      } catch (error) {
-        console.error(`\n${JSON.stringify(error)}`);
-        return null;
-      }
-
-      if (revision < 171 || (revision > 170 && !webglModule)) {
-        /** Remove color keywords */
-        if (!options.colorKeywords) code = removeColorKeywords(code);
-
-        /** Remove `toJSON` and `fromJSON` methods on all classes */
-        if (!options.jsonMethods) {
-          code = code.replace(
-            /^(\s+)(static\s+)?(to|from)JSON\(.*?\)\s*{[\s\S]+?^\1}/gm,
-            '',
+    load: {
+      order: 'pre',
+      filter: {
+        id: threeRegex,
+      },
+      async handler(id) {
+        if (id.endsWith('.min.js')) {
+          console.warn(
+            `"${name}" does not work with minified versions of THREE.js`,
           );
+          return null;
         }
-      }
 
-      if (!webglModule) return code;
+        const webglModule = id.endsWith('three.module.js');
 
-      /** `parsedPath.endsWith('three.module.js')` */
+        let code: string;
 
-      /** Remove background stuff */
-      if (!options.background) code = removeBackground(code);
-
-      /** Remove clipping stuff */
-      if (!options.clipping) code = removeClipping(code);
-
-      /** Remove environment stuff */
-      if (!options.environment) code = removeEnvironment(code);
-
-      /** Remove shadow stuff */
-      if (!options.shadows) code = removeShadows(code);
-
-      /** Remove texture stuff */
-      if (!options.textures) code = removeTextures(code);
-
-      /** Remove WebXR stuff */
-      if (!options.xr) code = removeXR(code);
-
-      const keepShaders: string[] = [];
-      const unwantedIncludes: IncludeName[] = [];
-
-      for (const include of includes) {
-        if (options.includes.has(include)) {
-          keepShaders.push(include);
-        } else {
-          unwantedIncludes.push(include);
+        try {
+          code = await readFile(id, 'utf8');
+        } catch (error) {
+          console.error(`\n${JSON.stringify(error)}`);
+          return null;
         }
-      }
 
-      code = pruneShaders(
-        code,
-        options.materials,
-        keepShaders,
-        unwantedIncludes,
-      );
+        if (revision < 171 || (revision > 170 && !webglModule)) {
+          /** Remove color keywords */
+          if (!options.colorKeywords) code = removeColorKeywords(code);
 
-      /** Minify *wanted* shader chunks */
-      const shaders = code.matchAll(
-        new RegExp(`(?:${keepShaders.join('|')}) = ("([\\s\\S]+?)");\\n`, 'g'),
-      );
+          /** Remove `toJSON` and `fromJSON` methods on all classes */
+          if (!options.jsonMethods) {
+            code = code.replace(
+              /^(\s+)(static\s+)?(to|from)JSON\(.*?\)\s*{[\s\S]+?^\1}/gm,
+              '',
+            );
+          }
+        }
 
-      for (const shader of shaders) {
-        const shaderCode = shader[1];
+        if (!webglModule) return code;
 
-        const rawShaderCode = shader[2]
-          .replace(/\\n/g, '\n')
-          .replace(/\\t/g, '\t');
+        if (!options.background) code = removeBackground(code);
 
-        code = code.replace(shaderCode, `\`\n${minifyGLSL(rawShaderCode)}\``);
-      }
+        if (!options.clipping) code = removeClipping(code);
 
-      /** Minify inline shader code */
-      const inlineShaders = code.matchAll(/\/\* glsl \*\/\s*`([\s\S]+?)`/g);
+        if (!options.environment) code = removeEnvironment(code);
 
-      for (const shader of inlineShaders) {
-        const shaderCode = shader[1];
+        if (!options.shadows) code = removeShadows(code);
 
-        code = code.replace(shaderCode, minifyGLSL(shaderCode));
-      }
+        if (!options.textures) code = removeTextures(code);
 
-      return code;
+        if (!options.xr) code = removeXR(code);
+
+        const keepShaders: string[] = [];
+        const unwantedIncludes: IncludeName[] = [];
+
+        for (const include of includes) {
+          if (options.includes.has(include)) {
+            keepShaders.push(include);
+          } else {
+            unwantedIncludes.push(include);
+          }
+        }
+
+        code = pruneShaders(
+          code,
+          options.materials,
+          keepShaders,
+          unwantedIncludes,
+        );
+
+        /** Minify *wanted* shader chunks */
+        const shaders = code.matchAll(
+          new RegExp(
+            `(?:${keepShaders.join('|')}) = ("([\\s\\S]+?)");\\n`,
+            'g',
+          ),
+        );
+
+        for (const shader of shaders) {
+          const shaderCode = shader[1];
+
+          const rawShaderCode = shader[2]
+            .replace(/\\n/g, '\n')
+            .replace(/\\t/g, '\t');
+
+          code = code.replace(shaderCode, `\`\n${minifyGLSL(rawShaderCode)}\``);
+        }
+
+        /** Minify inline shader code */
+        const inlineShaders = code.matchAll(/\/\* glsl \*\/\s*`([\s\S]+?)`/g);
+
+        for (const shader of inlineShaders) {
+          const shaderCode = shader[1];
+
+          code = code.replace(shaderCode, minifyGLSL(shaderCode));
+        }
+
+        return code;
+      },
     },
   };
 }
