f1dabfadf1e7c67f68eb3b2ae0b6666a180e8597
Matt Bryant <brybrant@gmail.com>
2026-04-23 01:57:18 +0100

    Build synchronously to improve memory usage (seems to have practically no difference in performance versus asynchrnous processing of bundles)

--- test/rollx.js
+++ test/rollx.js
@@ -3,7 +3,6 @@
  * - `npm run rollup`
  * - `npm run rolldown`
  */
-
 import { env } from 'node:process';
 
 import html from '@rollup/plugin-html';
@@ -15,84 +14,80 @@ import { globalPlugin, revision } from './global-plugin.js';
 /** @type {'up'|'down'} */
 const roll = env.ROLL;
 
+const resolvePlugin = roll === 'up' ? nodeResolve() : {};
+
 /**
  * @param {string} name
  * @param {import('rollup-plugin-three-minify').UserOptions} options
- * @returns {import('rollup').RollupOptions} Rollup config options
+ * @returns {{ name: string, config: import('rollup').RollupOptions }} config
  */
 function createConfig(name, options) {
   return {
-    input: `./test/rollx/src/${name}.js`,
-    output: {
-      file: `./test/rollx/dist/${name}.js`,
+    name,
+    config: {
+      input: `./test/rollx/src/${name}.js`,
+      output: {
+        file: `./test/rollx/dist/${name}.js`,
+      },
+      plugins: [
+        globalPlugin,
+        resolvePlugin,
+        html({
+          fileName: `${name}.html`,
+          template: () => `
+          <!doctype html>
+          <html>
+            <head>
+              <meta charset='utf-8'>
+              <meta name='darkreader-lock'/>
+              <title>${name}</title>
+              <link href='../../index.css' rel='stylesheet'/>
+            </head>
+            <body>
+              <script src='${name}.js'></script>
+            </body>
+          </html>`,
+        }),
+        threeMinifyPlugin(options),
+      ],
     },
-    plugins: [
-      globalPlugin,
-      ...(roll === 'up' ? [nodeResolve()] : []),
-      html({
-        fileName: `${name}.html`,
-        template: () => `
-        <!doctype html>
-        <html>
-          <head>
-            <meta charset='utf-8'>
-            <meta name='darkreader-lock'/>
-            <title>${name}</title>
-            <link href='../../index.css' rel='stylesheet'/>
-          </head>
-          <body>
-            <script src='${name}.js'></script>
-          </body>
-        </html>`,
-      }),
-      threeMinifyPlugin(options),
-    ],
   };
 }
 
 const configs = [
   createConfig('basic', {
-    textures: true,
     features: 'map',
     materials: 'basic',
   }),
   createConfig('depth', {
-    textures: true,
     features: ['alphamap', 'alphatest'],
     materials: 'depth',
   }),
   createConfig('lambert', {
-    textures: true,
     features: 'emissivemap',
     materials: 'lambert',
   }),
   createConfig('normal', {
-    textures: true,
     features: ['dithering', 'normalmap'],
     materials: 'normal',
   }),
   createConfig('phong', {
-    environment: true,
+    features: 'envmap',
     materials: 'phong',
   }),
   createConfig('physical', {
-    background: true,
-    environment: true,
-    includes: 'cube_uv_reflection_fragment',
+    features: 'envmap',
     materials: ['physical', revision < 146 ? 'cube' : 'backgroundCube'],
   }),
   createConfig('shadow', {
-    shadows: true,
-    features: ['bumpmap', 'lightmap'],
+    features: ['bumpmap', 'shadows'],
     materials: ['lambert', 'phong'],
   }),
   createConfig('standard', {
-    environment: true,
-    includes: 'cube_uv_reflection_fragment',
+    features: 'envmap',
     materials: 'standard',
   }),
   createConfig('toon', {
-    textures: true,
     features: 'lightmap',
     materials: 'toon',
   }),
@@ -106,17 +101,8 @@ const bundler = await import(`roll${roll}`).then(
   (module) => module[`roll${roll}`],
 );
 
-Promise.all(configs.map((config) => bundler(config)))
-  .then((builds) => {
-    /** @type {Promise<void>[]} */
-    const finished = [];
-
-    for (let i = 0; i < builds.length; i++) {
-      const build = builds[i];
-
-      finished.push(build.write(configs[i].output).then(() => build.close));
-    }
-
-    return Promise.all(finished);
-  })
-  .finally(() => console.log('Finised!'));
+for (const config of configs) {
+  const build = await bundler(config.config);
+  await build.write(config.config.output);
+  await build.close();
+}
